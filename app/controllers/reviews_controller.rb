b67b89808327d9d2b6045a4ab3f32bd0b1cddd8f
Azhar Ollibokus <azhar4@gmail.com>
2023-02-11 16:44:18 +0400

    controllers edited

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,22 @@
 class ReviewsController < ApplicationController
+  def new
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(review_params)
+    @review.user = current_user
+    @review.package_id = Booking.find(params[:booking_id]).package_id
+    if @review.save
+      redirect_to "/thankyou"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:booking_id, :user_id)
+  end
 end
