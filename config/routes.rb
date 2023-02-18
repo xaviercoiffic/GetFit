d1b95d2db3afa7bcc8accc4ca6e604d371b67997
neildus <dussoye.neil@gmail.com>
2023-02-18 18:57:28 +0400

    routes really working this time

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
-
   resources :users
+  resources :packages
+  resources :bookings do
+    resources :reviews, only: [:index, :new, :create]
+  end
+  resources :reviews, only: [:show, :destroy, :update, :edit]
 
+get 'index' => 'users#index'
 end
