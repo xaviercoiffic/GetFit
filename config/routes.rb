00a4abdfe98c508f4cb8a5937c846c2d8a7c9455
Xavier Coiffic <xavier@woho.io>
2023-02-18 18:49:39 +0400

    routes the real fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "products#index"
-  get 'about', to: 'pages#about'
-  get 'account', to: 'pages#account'
-  get 'yoda', to: 'pages#yoda'
-  get 'thankyou', to: 'pages#thankyou'
+  root to: "pages#home"
 
   resources :users
-  resources :products
-  resources :bookings do
-    resources :ratings, only: [:index, :new, :create]
-  end
-  resources :ratings, only: [:show, :destroy, :update, :edit]
+
 end
