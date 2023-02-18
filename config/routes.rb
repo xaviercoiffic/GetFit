7f944fb23a1a5c35a483d04fbc7287f55419e162
Xavier Coiffic <xavier@woho.io>
2023-02-18 23:37:08 +0400

    lots of adjustments

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
+  get 'pofile', to: 'pages#profile'
+
   resources :users
   resources :packages
   resources :bookings do
