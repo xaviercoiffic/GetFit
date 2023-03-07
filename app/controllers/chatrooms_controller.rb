a23749db94f1bc7354ab78d24c31803feeb438a6
Xavier Coiffic <xavier@woho.io>
2023-03-07 19:28:22 +0400

    Neils chat is working

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -1,21 +1,9 @@
 class ChatroomsController < ApplicationController
-  # def show
-  #   @chatroom = Chatroom.find(params[:id])
-  #   @message = Message.new
-  # end
 
   def index
-    @chatrooms = policy_scope(Chatroom)
-    # Only show chatrooms where a message has been sent
-    @chatrooms = @chatrooms.select { |chatroom| chatroom.messages.any? }
-                            .sort_by { |chatroom| chatroom.messages.last.created_at }
-    @chatroom = Chatroom.new
-    user_chatrooms = @chatrooms.select do |chatroom|
-      chatroom.chatroom_user.any? do |chatroom_user|
-        chatroom_user.user_id == current_user.id
-      end
-    end
-    @chatrooms_count = user_chatrooms.count
+    @chatrooms = Chatroom.where(client_id: current_user.id)
+    @chatrooms = @chatrooms.select { |chatroom| chatroom.messages.any? }.sort_by { |chatroom| chatroom.messages.last.created_at }
+    @chatrooms_count = @chatrooms.count
   end
 
   def show
@@ -32,15 +20,13 @@ class ChatroomsController < ApplicationController
   end
 
   def create
-    @user = User.find(params[:user])
+    @user = User.find(params[:user_id])
     @current_user = current_user
-   #@rooms = Chatroom.public_rooms
     @room = Chatroom.new
     @room_name = get_name(@user, @current_user)
-    @single_room = Chatroom.where(name: @room_name).first || Chatroom.create_private_room([@user, @current_user], @room_name)
+    @single_room = Chatroom.where(name: @room_name).first || Chatroom.create(user: @user, name: @room_name, client_id: @current_user.id)
     @messages = @single_room.messages
     redirect_to chatroom_path(@single_room)
-    authorize @single_room
   end
 
   private
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_05_163358) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_07_152453) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -45,8 +45,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_05_163358) do
   create_table "chatrooms", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "user_id"
     t.string "name"
+    t.bigint "user_id", null: false
+    t.integer "client_id"
     t.index ["user_id"], name: "index_chatrooms_on_user_id"
   end
 
@@ -118,6 +119,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_05_163358) do
   add_foreign_key "bookings", "users"
   add_foreign_key "chatroom_users", "chatrooms"
   add_foreign_key "chatroom_users", "users"
+  add_foreign_key "chatrooms", "users"
   add_foreign_key "goals", "users"
   add_foreign_key "messages", "chatrooms"
   add_foreign_key "messages", "users"
