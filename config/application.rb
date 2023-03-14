3e9b6ebaf9b0dbba9d366bf8b9fa9d03046975a0
Xavier Coiffic <xavier@woho.io>
2023-03-14 14:07:26 +0400

    Frontend - improved overall experience

--- config/application.rb
+++ config/application.rb
@@ -23,7 +23,7 @@ module GetFit
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    # config.time_zone = "Central Time (US & Canada)"
+    config.time_zone = 'Indian/Mauritius'
     # config.eager_load_paths << Rails.root.join("extras")
   end
 end
